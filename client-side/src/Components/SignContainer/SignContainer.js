4124705913eef6d5d04d0874e924804b028cc641
vinciusb <65973642+vinciusb@users.noreply.github.com>
2021-05-28 18:50:16 -0300

    Third commit, new sign in and sign up interface.

--- client-side/src/Components/SignContainer/SignContainer.js
+++ client-side/src/Components/SignContainer/SignContainer.js
@@ -1,5 +1,6 @@
 import './SignContainer.css';
 import React from 'react';
+import Arrow from './arrow.png'
 
 class SignContainer extends React.Component {
     constructor(props) {
@@ -7,52 +8,51 @@ class SignContainer extends React.Component {
 
         this.state = {
             signingIn: true,
-            failed: null
+            msg: null
         };
 
-        this.handleSignIn = this.handleSignIn.bind(this);
-        this.handleSignUp = this.handleSignUp.bind(this);
-        this.changeToSignUp = this.changeToSignUp.bind(this);
+        this.handleSign = this.handleSign.bind(this);
+        this.changeSign = this.changeSign.bind(this);
     }
 
-    handleSignIn(data) {
-        const url = `http://localhost:4001/signin?email=${data.email}&pwd=${data.password}`;
-        fetch(url)
-            .then(res => res.ok ?
-                this.props.onSignIn() : res.text().then(msg => { throw new Error(msg); })
-            )
+    handleSign(data) {
+        var url;
+        var method;
+        if(this.state.signingIn) {
+            url = `http://localhost:4001/signin?email=${data.email}&pwd=${data.password}`;
+            method = {};
+        }
+        else {
+            url = 'http://localhost:4001/signup';
+            method = {
+                method: 'PUT',
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(data)
+            };
+        }
+
+        fetch(url, method)
+            .then(res => {
+                if(res.ok) this.state.signingIn && this.props.onSignIn();
+                else res.text().then(msg => this.setState({ msg }));
+            })
             .catch(err => {
                 console.log(err.message);
             });
     }
 
-    handleSignUp(data) {
-        const putMethod = {
-            method: 'PUT',
-            headers: { 'Content-type': 'application/json; charset=UTF-8' },
-            body: JSON.stringify(data)
-        };
-            
-        fetch('http://localhost:4001/signup', putMethod)
-            .then(res => res.ok ?
-                res.json() : res.text().then(msg => { throw new Error(msg); })
-            )
-            .then(res => console.log(res))
-            // .then(this.props.onSignIn())
-            .catch(err => {
-                console.log(err.message);
-            });
-    }
-
-    changeToSignUp() {
-        this.setState({ signingIn: false });
+    changeSign() {
+        this.setState({
+            signingIn: !this.state.signingIn,
+            msg: null
+        });
     }
 
     render() {
         return (
             <div className="SignContainer">
-                <Sign type={ this.state.signingIn } failMsg={"a"} onSubmit={ this.state.signingIn ? this.handleSignIn : this.handleSignUp } />
-                { this.state.signingIn && <p>Not a member yet? <a onClick={ this.changeToSignUp }>Sign up</a></p> }
+                <Sign type={ this.state.signingIn } failMsg={ this.state.msg } onSubmit={ this.handleSign } onChangeSign={ this.changeSign } />
+                { this.state.signingIn && <h3>Not a member yet? <a onClick={ this.changeSign }>Sign up</a></h3> }
             </div>
         );
     }
@@ -69,7 +69,8 @@ class Sign extends React.Component {
                 lName: "",
                 email: "",
                 password: ""
-            }
+            },
+            mouseOn: false
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -92,20 +93,22 @@ class Sign extends React.Component {
         let children;
         if(type) {
             children =
-                <form action="" onSubmit={ this.handleSubmit } >
+                <form className="sign-form" onSubmit={ this.handleSubmit } >
                     <input name="email" type="text" placeholder="E-mail" value={ this.state.formData.email } onChange={ this.handleChange } />
                     <input name="password" type="password" placeholder="Password" value={ this.state.formData.password } onChange={ this.handleChange } />
-                    <input type="submit" value="Sign In" />
+                    <input className="submit-button" type="submit" value="Sign In" />
+                    { this.props.failMsg !== null && <div className="signError"><h3>{ this.props.failMsg }</h3></div> }
                 </form>;
         }
         else {
             children =
-                <form action="" onSubmit={ this.handleSubmit } >
-                <input name="fName" type="text" placeholder="First name" minLength="1" maxLength="20" value={ this.state.formData.fName } onChange={ this.handleChange } required={ true } />
+                <form className="sign-form" onSubmit={ this.handleSubmit } >
+                    <input name="fName" type="text" placeholder="First name" minLength="1" maxLength="20" value={ this.state.formData.fName } onChange={ this.handleChange } required={ true } />
                     <input name="lName" type="text" placeholder="Last name" minLength="1" maxLength="20" value={ this.state.formData.lName } onChange={ this.handleChange } required={ true } />
                     <input name="email" type="text" placeholder="E-mail" minLength="4" value={ this.state.formData.email } onChange={ this.handleChange } required={ true } />
                     <input name="password" type="password" placeholder="Password" minLength="8" maxLength="20" value={ this.state.formData.password } onChange={ this.handleChange } required={ true } />
-                    <input type="submit" value="Sign Up" />
+                    <input className="submit-button" type="submit" value="Sign Up" onMouseOut={ this.animateOut } />
+                    { this.props.failMsg !== null && <div className="signError"><h3>{ this.props.failMsg }</h3></div> }
                 </form>;
         }
         return children;
@@ -114,6 +117,7 @@ class Sign extends React.Component {
     render() {
         return (
             <div className="Sign">
+                { !this.props.type && <img className="back-arrow" src={ Arrow } alt="back arrow" onClick={ this.props.onChangeSign } />}
                 { this.renderSign(this.props.type) }
             </div>
         );
