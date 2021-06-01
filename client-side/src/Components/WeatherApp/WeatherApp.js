23b648c148daf60de802472a016e7e2fa89143a6
vinciusb <65973642+vinciusb@users.noreply.github.com>
2021-06-01 20:00:04 -0300

    Sixth commit, background color response based on weather state.

--- client-side/src/Components/WeatherApp/WeatherApp.js
+++ client-side/src/Components/WeatherApp/WeatherApp.js
@@ -16,27 +16,82 @@ class WeatherApp extends React.Component {
             timeFormat: false,
             filter: {
                 sat: 0.4,
-                hue: 0,
+                hue: 100,
                 brightness: 75
-            }
+            },
+            searchSuccess: true
         };
 
+        this.getFilterString = this.getFilterString.bind(this);
+        this.handleWeatherChange = this.handleWeatherChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    getFilterString() {
+        return {
+            "--filter": `saturate(${this.state.filter.sat}) hue-rotate(${this.state.filter.hue}deg) brightness(${this.state.filter.brightness}%)`
+        };
+    }
+
+    handleWeatherChange(weatherState) {
+        var sat = 0.4;
+        var hue = 0;
+        var brightness = 75;
+
+        if(weatherState === "CLEAR") {
+            sat = 0.6;
+            hue = 30;
+        }
+        else if(weatherState === "FEW CLOUDS" || weatherState === "SCATTERED CLOUDS" || weatherState === "BROKEN CLOUDS" || weatherState === "CLOUDS") {
+            sat = 0.25;
+            hue = 200;
+        }
+        else if(weatherState === "RAIN" || weatherState === "SHOWER RAIN") {
+            sat = 0.45;
+            hue = 180;
+        }
+        else if(weatherState === "THUNDERSTORM") {
+            brightness = 50;
+            hue = 270;
+        }
+        else if(weatherState === "SNOW") {
+            hue = 200;
+            sat = 0.7;
+            brightness = 60;
+        }
+        else if(weatherState === "MIST") {
+            sat = 0.0;
+            brightness = 60;
+        }
+
+        const filter = {
+            sat: sat,
+            hue: hue,
+            brightness: brightness
+        };
+        this.setState({ filter });
     }
 
     handleSearch(text) {
         weatherAPI.searchWeather(text)
-            .then(data => this.setState({ weatherResult: data },
-                () => {
-                    weatherAPI.searchForecast(text)
-                        .then(data => this.setState({ forecastResult: data }));
-                }
-            ));
+            .then(data => {
+                if(data.cod === "404") { throw new Error(data.message); };
+                this.setState({ weatherResult: data },
+                    () => {
+                        // Change background color
+                        this.handleWeatherChange(this.state.weatherResult.weather[0].main.toUpperCase());
+                        // Search forecasts
+                        weatherAPI.searchForecast(text)
+                            .then(data => this.setState({ forecastResult: data }));
+                    }
+                );
+            })
+            .catch(err => console.log(err.message));
     }
 
     render() {
         return (
-            <div className="WeatherApp" beo>
+            <div className="WeatherApp" style={ this.getFilterString() }>
                 <div className="WeatherApp-inside">
                     <header>
                         <SearchBar onSearch={ this.handleSearch } />
